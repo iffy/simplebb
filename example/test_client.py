3f41983ea461d22ed57c3945bb5b38cbe4f9b342
Matt Haggard <haggardii@gmail.com>
2011-06-15 18:31:00 -0600

    Added an example server-only and client-only scripts for functional testing

---
+++ example/test_client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+from twisted.internet import task
+from twisted.python import log
+from twisted.internet import reactor
+import sys
+
+log.startLogging(sys.stdout)
+
+from simplebb.hub import Hub
+
+h = Hub()
+h.connect('tcp:host=127.0.0.1:port=8100')
+
+
+def monitor():
+    log.msg(h._builders)
+    log.msg(h._clients)
+    log.msg(h._servers)
+t = task.LoopingCall(monitor)
+t.start(5)
+
+reactor.run()
