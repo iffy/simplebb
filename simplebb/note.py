e6e7a14f6d3ea9344e327c9ac1a7ba063d824175
Matt <haggardii@gmail.com>
2011-08-31 11:25:25 -0600

    Some cleaning

--- simplebb/note.py
+++ simplebb/note.py
@@ -10,6 +10,15 @@ from zope.interface import implements
 
 
 
+def makeUID():
+    """
+    Returns a new random hash
+    """
+    return hashlib.sha256(hex(random.getrandbits(1024)) + repr(id([])))
+
+
+
+
 class BuildRequest:
     """
     I am a request to build a certain version of a project.
@@ -22,8 +31,7 @@ class BuildRequest:
     version = None
 
     def __init__(self, project=None, version=None, uid=None):
-        self.uid = uid or hashlib.sha256(hex(random.getrandbits(256)) + \
-                                         repr(id([])))
+        self.uid = uid or makeUID()
         self.project = project
         self.version = version
 
@@ -45,8 +53,7 @@ class Note:
 
 
     def __init__(self):
-        self.uid = hashlib.sha256(hex(random.getrandbits(256)) + \
-                                  repr(id([])))
+        self.uid = makeUID()
 
 
 
